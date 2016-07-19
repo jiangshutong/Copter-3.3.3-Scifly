d6d756b586aade810ce75a8e9f2347a25e8ed543
Steven Jiang <jiangshu.1330@gmail.com>
2016-07-19 21:53:14 +0800

    minor change to the motor matrix - updated yaw matrix

--- libraries/AP_Motors/AP_MotorsQuad.cpp
+++ libraries/AP_Motors/AP_MotorsQuad.cpp
@@ -89,10 +89,10 @@ void AP_MotorsQuad::setup_motors()
         add_motor(AP_MOTORS_MOT_3, -60, -60, 0, 4);
         add_motor(AP_MOTORS_MOT_4, 0, 160, AP_MOTORS_MATRIX_YAW_FACTOR_CW, 2);
     } else if (_flags.frame_orientation == AP_MOTORS_NEW_X_FRAME) { //the frame used in Precision prototype v3
-        add_motor_raw(AP_MOTORS_MOT_1,  -0.89113f,   0.89109f,  AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 1);
-        add_motor_raw(AP_MOTORS_MOT_2,  1.0f,       -1.0f,     AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 3);
-        add_motor_raw(AP_MOTORS_MOT_3,  0.89113f,    0.89109f,  AP_MOTORS_MATRIX_YAW_FACTOR_CW,  4);
-        add_motor_raw(AP_MOTORS_MOT_4,  -1.0f,      -1.0f,    AP_MOTORS_MATRIX_YAW_FACTOR_CW,  2);
+        add_motor_raw(AP_MOTORS_MOT_1,  -0.89113f,   0.89109f,   0.89111,   1);// at 52.13 degrees, arm length 362.11 mm
+        add_motor_raw(AP_MOTORS_MOT_2,  1.0f,       -1.0f,       1,         3);// at -127.87 degrees, arm length 322.68 mm
+        add_motor_raw(AP_MOTORS_MOT_3,  0.89113f,    0.89109f,   -0.89111,   4);// at -52.13 degrees, arm length 362.11 mm
+        add_motor_raw(AP_MOTORS_MOT_4,  -1.0f,      -1.0f,       -1,         2);// at 127.87 degrees, arm length 322.68 mm
     }else{
         // conventional X frame set-up
         add_motor(AP_MOTORS_MOT_1,   45, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 1);
